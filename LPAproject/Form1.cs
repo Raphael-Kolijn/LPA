7c41e281473d334962aee397754b98c29d8edd70
Raphael Kolijn <raphaelkolijn@gmail.com>
2016-06-20 11:16:36 +0200

    test

--- LPAproject/Form1.cs
+++ LPAproject/Form1.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using LPAproject.Properties;
 
-namespace LPAproject
+namespace LPAproject  // Test Text
 {
     public partial class Form1 : Form
     {
